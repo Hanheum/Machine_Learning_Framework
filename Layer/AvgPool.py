d22c7728c9a0bc10de6b313879268290f54bd1b3
Hanheum Cho <chh3653@gmail.com>
2023-08-12 12:54:19 +0900

    average Pool added, CCE fixed, change coefficient multiplied to cce deriv when it's broken

---
+++ Layer/AvgPool.py
@@ -0,0 +1,43 @@
+from Layer.Default import layer
+import numpy as np
+from scipy.signal import correlate
+
+class AvgPool(layer):
+    def __init__(self, kernel_size, input_shape=None):
+        super().__init__(input_shape=input_shape)
+
+        self.kernel_size = kernel_size
+        self.K = np.ones(kernel_size)
+
+    def forward(self, X):
+        self.X = X
+        self.Z = np.zeros((len(self.X), *self.output_shape))
+        for a, one_x in enumerate(self.X):
+            for i in range(one_x.shape[0]):
+                self.Z[a][i] = correlate(one_x[i], self.K, mode='valid', method='direct')
+        self.A = self.Z
+        return self.Z.astype(np.float32), self.A.astype(np.float32)
+
+    def backward(self, dZ1):
+        dX = np.zeros_like(self.X)
+        for a, one_dz_next in enumerate(dZ1):
+            for i in range(self.input_shape[0]):
+                dX[a][i] = correlate(one_dz_next[i], self.K, mode='full', method='direct')
+        return dX.astype(np.float32)
+    
+    def __call__(self, X):
+        self.X = X.astype(np.float32)
+        Z, A = self.forward(self.X)
+        return Z, A
+    
+    def optimize(self, optimize_function, learning_rate):
+        pass
+    
+    def calculate_output_shape(self):
+        self.output_shape = (self.input_shape[0], self.input_shape[-1]-self.kernel_size[0]+1, self.input_shape[-1]-self.kernel_size[0]+1)
+    
+    def make_variables(self):
+        pass
+
+    def reset_gradient(self):
+        pass
--- Loss/CCE.py
+++ Loss/CCE.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 def CCE_op(Y, Y_pred):
-    Y_pred = np.clip(Y_pred, 1e-7, 1 - 1e-7)
-    return -np.mean(Y * np.log(Y_pred + 1e-7) + (1 - Y) * np.log(1 - Y_pred + 1e-7))
+    Y_pred = np.clip(Y_pred, 1e-5, 1 - 1e-5)
+    return -np.mean(Y * np.log(Y_pred + 1e-5) + (1 - Y) * np.log(1 - Y_pred + 1e-5))
 
 def CCE_deriv(Y, Y_pred):
-    Y_pred = np.clip(Y_pred, 1e-7, 1 - 1e-7)
-    cost = ((1 - Y) / (1 - Y_pred + 1e-7) - Y / (Y_pred + 1e-7)) / np.size(Y)
-    return cost * 1000
+    Y_pred = np.clip(Y_pred, 1e-5, 1 - 1e-5)
+    cost = ((1 - Y) / (1 - Y_pred + 1e-5) - Y / (Y_pred + 1e-5)) / np.size(Y)
+    return cost/1000
 
 def CCE():
     return CCE_op, CCE_deriv
